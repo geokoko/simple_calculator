d8d62bf45466c4e7d2770402b61612f7285c6562
geokoko <geokoko3@outlook.com.gr>
2023-11-26 15:02:40 +0200

    Changed the tokenization function - added support for unary operator

--- math_expr.py
+++ math_expr.py
@@ -1,46 +1,44 @@
+operators = {'+': (1, 'left', lambda x, y: x + y), 
+             '-': (1, 'left', lambda x, y: x - y),
+             '*': (2, 'left', lambda x, y: x * y),
+             '/': (2, 'left', lambda x, y: x / y),
+             'u': (3, 'right', lambda x: -x)}
+
 def is_float(string):
     try:
         float(string)
         return True
     except ValueError:
         return False
-
-def calc(expression):
-    operators = {'+': (1, lambda x, y: x + y), 
-                 '-': (1, lambda x, y: x - y),
-                 '*': (2, lambda x, y: x * y),
-                 '/': (2, lambda x, y: x / y)}
     
-    def infix_to_postfix():
-        tokens = []
-        current_token = ''
-        #tokenize the expression
-        for char in expression:
-            if char.isdigit() or char == '.':
-                current_token += char
-            elif char in operators or char in ['(', ')']:
-                if current_token:
-                    tokens.append(current_token)
-                tokens.append(char)
-                current_token = ''
-        if current_token:
-            tokens.append(current_token)
+def tokenize(expression):
+    tokens = []
+    current_token = ''
+    #tokenize the expression
+    for char in expression:
+        if char.isdigit() or char == '.':
+            current_token += char
+        elif char in operators or char in ['(', ')']:
+            if current_token:
+                tokens.append(current_token)
+            tokens.append(char)
+            current_token = ''
+    if current_token:
+        tokens.append(current_token)
+    
+    for i, token in enumerate(tokens):
+        if token == '-' and (i == 0 or (tokens[i - 1] in operators or tokens[i - 1] in ['(',')'])):
+            tokens[i] = 'u'
 
-        #handling unary - operator
-        if tokens[0] == '-':
-            tokens.insert(0, '0')
-            tokens.insert(0, '(')
-            tokens.insert(4, ')')
-        
-        n = len(tokens)
-        for i in range(n - 1):
-            if tokens[i] == '(' and tokens[i + 1] == '-':
-                tokens.insert(i + 1, 0)
-            
+    print(tokens)
+
+    return tokens
+
+def calc(expression):  
+    def infix_to_postfix():
+        tokens = tokenize(expression)
         postfix_list = []
         operator_stack = []
-        print(tokens)
-
         for token in tokens:
             if is_float(token):
                 postfix_list.append(token)
@@ -59,8 +57,7 @@ def calc(expression):
         while operator_stack:
             postfix_list.append(operator_stack.pop())
             
-            
-        return ''.join(postfix_list)
+        return (postfix_list)
         
     print(infix_to_postfix())
         
